02a60d53037c9ab3f2fa69bf9f1fc4b9984fbb34
Samip <batman@192-168-1-102.tpgi.com.au>
2021-09-06 21:35:41 +1000

    Everything except Delete working

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express"); //requiring express
+const { METHODS } = require("http");
 const app = express(); //executing express
 const path = require("path"); //gets the current path 
 const { cwd } = require("process");
+const methodOverride = require("method-override");
 app.set("view engine", "ejs")
 app.set("views", (path.join(__dirname, "/views")));
 app.use(express.static(path.join(__dirname, "/public")));
 const { v4: getID } = require('uuid');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 const date = new Date().toDateString();
 let userComments = [
     {
@@ -42,6 +45,9 @@ app.get("/", (req, res) => {
     res.render("dashboard.ejs",{userComments});
     
 });
+app.post("/", (req, res) => {
+    res.render("dashboard.ejs",{userComments});
+});
 
 app.get("/edit/:id", (req, res) => {
     const { id } = req.params;
@@ -49,7 +55,20 @@ app.get("/edit/:id", (req, res) => {
     console.log(comment);
     res.render("edit",{comment})
 });
-  
+
+app.patch("/edit/:id", (req, res) => {
+    const { id,says } = req.params;
+    const comment = userComments.find(c => c.id === id);
+    const newComment = req.body.says;
+    comment.said = newComment;
+    res.redirect("/")
+});
+app.delete("/edit/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = userComments.filter(c => c.id !== id);
+    res.redirect("/")
+});
+
 app.get("/new", (req, res) => {
     res.render("createNewPost.ejs");
     
@@ -60,6 +79,7 @@ app.post("/new", (req, res) => {
     userComments.push({ username, said:says, id: getID(), postedOn: date });
     res.redirect("/");
 })
+
 
 app.listen(3000, () => {
   console.log("Server running ?");
