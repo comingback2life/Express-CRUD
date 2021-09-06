dff8b91bcbb45ce1e3e9074254f5c22c40b96e81
Samip <batman@192-168-1-102.tpgi.com.au>
2021-09-06 17:58:21 +1000

    Completed the HTML styling

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //requiring express
+const app = express(); //executing express
+const path = require("path"); //gets the current path 
+const { cwd } = require("process");
+app.set("view engine", "ejs")
+app.set("views", (path.join(__dirname, "/views")));
+app.use(express.static(path.join(__dirname, "/public")));
+const { v4: getID } = require('uuid');
+const date = new Date().toDateString();
+let userComments = [
+    {
+        id: getID(),
+        username: "Samip",
+        said: "Hi my name is Samip",
+        postedOn: date
+    },
+    {
+        id: getID(),
+        username: "Lenin",
+        said: "I make wonderful ramen",
+        postedOn: date
+    },
+    {
+        id: getID(),
+        username: "Hattori",
+        said: "Ding Ding Ding",
+        postedOn: date
+    },
+    
+    
+]
+
+app.get("/", (req, res) => {
+    res.render("dashboard.ejs",{userComments});
+    
+});
+app.get("/edit", (req, res) => {
+    res.render("edit.ejs");
+    
+});
+app.get("/new", (req, res) => {
+    res.render("createNewPost.ejs");
+    
+});
+
+app.listen(3000, () => {
+  console.log("Server running ?");
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 Simple application built using Node that makes blog posts.
 
 I have used an array to simplify things and has no database connectivity at all
+
+//Design the way you want it to work
+
+Home or index displays the blog posts in a concurrent manner
+Edit : Takes you to a page where comment can be edited
+Delete : Delete the comment
